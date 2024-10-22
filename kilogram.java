4004f8cbbf64d5f68f37071bfeea4479a742e7a7
Mia Dela Cruz <113849024+miadelacruz@users.noreply.github.com>
2024-10-22 18:50:57 -0400

    Add files via upload

---
+++ kilogram.java
@@ -0,0 +1,14 @@
+public class kilogram {
+    public static void main(String [] args)
+    {
+      
+        double pounds = 2.2;
+        int i;
+        for (int kilogram=1;kilogram<=200; kilogram++) {
+            pounds = kilogram * 2.2;
+            System.out.println(kilogram + " kilograms =  " + pounds + " pounds");
+        }
+        
+    }
+
+}
